c95c6e9da22764b7d7d37cb098b9c8543af17ed6
B0X <83914919+BOLTB0X@users.noreply.github.com>
2023-11-03 17:47:14 +0900

    gitignore add
    DS_Store 무시

---
+++ PhotoPickerSample/PhotoPickerSample/PhotoPickerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerSampleApp.swift
+//  PhotoPickerSample
+//
+//  Created by lkh on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
